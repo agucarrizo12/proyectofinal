1c8ef93e986660431effcdd57f24fde426aa2229
agucarrizo12 <105244566+agucarrizo12@users.noreply.github.com>
2022-09-16 01:12:29 -0300

    Add files via upload

---
+++ proyectofinal.sql
@@ -0,0 +1,19 @@
+
+create database proyectofinal;
+
+use proyectofinal;
+
+create table mensajes(
+nombre varchar(30),
+email varchar(30),
+mensaje varchar(200)
+);
+
+select * from mensajes;
+
+insert into mensajes values (null, "agustin", "hola");
+
+
+
+
+
